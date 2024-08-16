c0eaec0d3d1f36f8212278520695598bd12650bf
Tom Grant <thomas.grant.duck321@gmail.com>
2024-08-16 21:44:14 +0100

    Convert grid to 2D coordinates

--- Assets/TileGrid.cs
+++ Assets/TileGrid.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -20,12 +21,12 @@ class Tile
 
 public class TileGrid
 {
-    Tile[,,] tiles = new Tile[20, 20, 20];
+    Tile[,] tiles = new Tile[100, 100];
 
 
-    bool hasNeighbourOfState(State state, int x, int y, int z)
+    bool hasNeighbourOfState(State state, int x, int y)
     {
-        foreach (Tile neighbour in getNeighbors(x, y, z))
+        foreach (Tile neighbour in getNeighbors(x, y))
         {
             if (neighbour.state == state)
             {
@@ -35,40 +36,31 @@ public class TileGrid
         return false;
     }
 
-    List<Tile> getNeighbors(int x, int y, int z)
+    List<Tile> getNeighbors(int x, int y)
     {
+        List<(int, int)> offsets = new List<(int, int)>{
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1),
+            (-1, -1),
+            (-1, 1),
+        };
         List<Tile> neighbours = new List<Tile>();
-        if (x < tiles.GetLength(0))
+        foreach (var (dir_x, dir_y) in offsets)
         {
-            neighbours.Add(tiles[x + 1, y, z]);
-        }
-        if (x > 0)
-        {
-            neighbours.Add(tiles[x - 1, y, z]);
-        }
-        if (y < tiles.GetLength(0))
-        {
-            neighbours.Add(tiles[x, y + 1, z]);
-        }
-        if (y > 0)
-        {
-            neighbours.Add(tiles[x, y - 1, z]);
-        }
-        if (z < tiles.GetLength(0))
-        {
-            neighbours.Add(tiles[x, y, z + 1]);
-        }
-        if (z > 0)
-        {
-            neighbours.Add(tiles[x, y, z - 1]);
+            if (x + dir_x >= 0 && x + dir_x < tiles.GetLength(0) && y + dir_y >= 0 && y + dir_y < tiles.GetLength(1))
+            {
+                neighbours.Add(tiles[x + dir_x, y + dir_y]);
+            }
         }
         return neighbours;
     }
 
-    List<Tile> getNeighborsOfState(State state, int x, int y, int z)
+    List<Tile> getNeighborsOfState(State state, int x, int y)
     {
         List<Tile> neighbours = new List<Tile>();
-        foreach (Tile neighbour in getNeighbors(x, y, z))
+        foreach (Tile neighbour in getNeighbors(x, y))
         {
             if (neighbour.state == state)
             {
@@ -78,31 +70,31 @@ public class TileGrid
         return neighbours;
     }
 
-    public List<GameObject> GetNeighbours(int x, int y, int z)
+    public List<GameObject> GetNeighbours(int x, int y)
     {
         List<GameObject> neighbours = new List<GameObject>();
-        foreach (Tile neighbour in getNeighborsOfState(State.occupied, x, y, z))
+        foreach (Tile neighbour in getNeighborsOfState(State.occupied, x, y))
         {
             neighbours.Add(neighbour.occupier);
         }
         return neighbours;
     }
 
-    public bool TryAddTile(GameObject to_add, int x, int y, int z)
+    public bool TryAddTile(GameObject to_add, int x, int y)
     {
-        if (tiles[x, y, z].state != State.empty)
+        if (tiles[x, y].state != State.empty)
         {
             return false;
         }
 
         // Has neighbour?
-        if (!hasNeighbourOfState(State.occupied, x, y, y))
+        if (!hasNeighbourOfState(State.occupied, x, y))
         {
             return false;
         }
 
 
-        tiles[x, y, z] = new Tile
+        tiles[x, y] = new Tile
         {
             state = State.occupied,
             occupier = to_add
@@ -110,6 +102,4 @@ public class TileGrid
 
         return true;
     }
-
-
 }
