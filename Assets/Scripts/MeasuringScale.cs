4b3ebc4cccb590a7c846869bab04125bf4c2226a
Tom Grant <thomas.grant.duck321@gmail.com>
2024-08-17 16:08:27 +0100

    Add particles to scale

--- Assets/Scripts/MeasuringScale.cs
+++ Assets/Scripts/MeasuringScale.cs
@@ -17,10 +17,20 @@ public class MeasuringScale : MonoBehaviour
     public Transform BucketRHS;
     public Transform BucketLHS;
 
+    public GameObject MoneyParticleSystemObj;
+    ParticleSystem moneyEmitter;
+
+    public GameObject TaxParticleSystemObj;
+    ParticleSystem taxEmitter;
+
+    float lastAngle = 0;
+    public float MaxAngleChangePerFrame = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        moneyEmitter = MoneyParticleSystemObj.GetComponent<ParticleSystem>();
+        taxEmitter = TaxParticleSystemObj.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -28,18 +38,45 @@ public class MeasuringScale : MonoBehaviour
     {
         float diff = lhs - rhs;
         float normalizedDiff = diff / (Math.Min(Math.Abs(lhs), Math.Abs(rhs)) + 1);
-        float angle = (float)(180 * Math.Atan(normalizedDiff / Stickiness) / Math.PI);
+        float requestAngle = (float)(180 * Math.Atan(normalizedDiff / Stickiness) / Math.PI);
 
-        Beam.localEulerAngles = new Vector3(0, 0, angle);
+        if (Math.Abs(requestAngle - lastAngle) > MaxAngleChangePerFrame) {
+            lastAngle += Math.Sign(requestAngle - lastAngle) * MaxAngleChangePerFrame;
+        } else {
+            lastAngle = requestAngle;
+        }
+
+        Beam.localEulerAngles = new Vector3(0, 0, lastAngle);
+
         BucketRHS.position = BeamRHS.position;
+        TaxParticleSystemObj.transform.position = BeamRHS.position;
+
         BucketLHS.position = BeamLHS.position;
+        MoneyParticleSystemObj.transform.position = BeamLHS.position;
     }
 
-    public void FactoryScoreUpdate(int amount) {
-        lhs += amount;
+    public void FactoryScoreUpdate(int amount)
+    {
+        moneyEmitter.Emit(amount);
     }
 
-    public void AddTax(float amount) {
-        rhs += amount;
+    public void AddTax(int amount)
+    {
+        taxEmitter.Emit(amount);
+    }
+
+    public void MoneyCollected((bool, int) state)
+    {
+        var (isTaxMan, amount) = state;
+        if (isTaxMan)
+        {
+            rhs += amount;
+
+        }
+        else
+        {
+            lhs += amount;
+
+        }
     }
 }
