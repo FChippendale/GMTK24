7d6717c83b4b92aa44e836e68a71397c581c28ca
FChippendale <felix.chippendale@gmail.com>
2024-08-17 01:15:03 +0100

    add type colors based on factory type

--- Assets/Scripts/TileDrawer.cs
+++ Assets/Scripts/TileDrawer.cs
@@ -6,6 +6,8 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
+
 public class TileDrawer : MonoBehaviour
 {
     [SerializeField]
@@ -15,16 +17,26 @@ public class TileDrawer : MonoBehaviour
 
     public Vector3Int position;
 
+    private FactoryBehaviour.TraversalType traversalType;
+
+    Dictionary<FactoryBehaviour.TraversalType, Color> mapping = new Dictionary<FactoryBehaviour.TraversalType, Color>{
+        {FactoryBehaviour.TraversalType.constant_integer_amount, new Color(0.20392f, 0.34902f, 0.58431f, 1.0f)},
+        {FactoryBehaviour.TraversalType.sum_of_any_adjacent, new Color(0.89412f, 0.0f, 0.4f, 1.0f)}
+        // 03CEA4
+        // FB4D3D
+    };
+
     // Start is called before the first frame update
     void Start()
-    {
-
+    {   
+        traversalType = GetComponent<FactoryBehaviour>().traversalType;
+        drawTile(position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        drawTile(position);
+        
     }
 
     private void drawTile(Vector3Int target)
@@ -36,5 +48,9 @@ public class TileDrawer : MonoBehaviour
         }
 
         tilemap.SetTile(target, tileAsset);
+        tilemap.SetTileFlags(target, TileFlags.None);
+        tilemap.SetColor(target, mapping[traversalType]);
+        
+        // tilemap.SetColor(target, mapping[FactoryBehaviour.TraversalType.constant_integer_amount]);
     }
 }
