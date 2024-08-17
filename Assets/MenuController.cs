2704527c8cfd0c3b2851e7a695df60b91ff4f6db
Antros Economides <antroseconomides@hotmail.co.uk>
2024-08-17 20:07:48 +0300

    Add Main menu

---
+++ Assets/MenuController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button playButton;
+    public Button helpButton;
+    public Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(() => SceneManager.LoadScene("SampleScene"));
+        // TODO helpButton
+        quitButton.onClick.AddListener(() => Application.Quit());
+
+    }
+}
