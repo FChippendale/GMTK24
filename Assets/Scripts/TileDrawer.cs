4290a2839c93aacb6171300eab644f30eead1e8f
Tom Grant <thomas.grant.duck321@gmail.com>
2024-08-18 14:40:21 +0100

    Add animation to tile destroy

--- Assets/Scripts/TileDrawer.cs
+++ Assets/Scripts/TileDrawer.cs
@@ -21,17 +21,38 @@ public class TileDrawer : MonoBehaviour
     private float animationPhase;
     private float animationFrequency;
 
+    private bool IsCurrentlyBeingDestroyed = false;
+    public float DestroyFlashFrequency = 20.0f;
+    private float DestroyRequestStartTime = 0.0f;
+    public int FlashesTillDestroy = 3;
+    private float timeToDestroy = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         DrawTile();
         animationPhase = Random.Range(0, Mathf.PI);
         animationFrequency = Random.Range(0.9f, 1.1f);
+        timeToDestroy = -Mathf.PI / DestroyFlashFrequency + FlashesTillDestroy * (2.0f * Mathf.PI / DestroyFlashFrequency);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (IsCurrentlyBeingDestroyed)
+        {
+            if (Time.time - DestroyRequestStartTime >= timeToDestroy)
+            {
+                tilemap.SetTile(position, null);
+                Destroy(gameObject);
+            }
+            float alpha = 0.5f + 0.5f * Mathf.Cos(DestroyFlashFrequency * (Time.time - DestroyRequestStartTime));
+            tilemap.SetColor(position, color.WithAlpha(alpha));
+
+            // Pause any other animations
+            return;
+        }
+
         if (traversalType != FactoryBehaviour.TraversalType.unbreakable_starting_tile)
         {
             float alpha = 0.8f + Mathf.Sin(
@@ -55,7 +76,7 @@ public class TileDrawer : MonoBehaviour
 
     public void BreakingTile()
     {
-        tilemap.SetTile(position, null);
-        Destroy(gameObject);
+        IsCurrentlyBeingDestroyed = true;
+        DestroyRequestStartTime = Time.time;
     }
 }
