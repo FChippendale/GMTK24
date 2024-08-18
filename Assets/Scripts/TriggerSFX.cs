615f71445ab1ba366d13fba234dfe8087b344950
FChippendale <felix.chippendale@gmail.com>
2024-08-18 15:22:04 +0100

    add new SFX to manager and script

--- Assets/Scripts/TriggerSFX.cs
+++ Assets/Scripts/TriggerSFX.cs
@@ -6,7 +6,11 @@ public class TriggerSFX : MonoBehaviour
 {
     public AudioSource breakBlocks;
     public AudioSource invalidPlacement;
+    public AudioSource invalidPlacement2;
+    public AudioSource invalidPlacement3;
     public AudioSource placement;
+    public AudioSource placement2;
+    public AudioSource placement3;
     public AudioSource timerTick;
     public AudioSource timerHitZero;
 
@@ -14,7 +18,11 @@ public class TriggerSFX : MonoBehaviour
     {
         break_block,
         invalid_placement,
+        invalid_placement2,
+        invalid_placement3,
         placement,
+        placement2,
+        placement3,
         timer_tick,
         timer_zero,
     }
@@ -25,7 +33,11 @@ public class TriggerSFX : MonoBehaviour
         {
             {SoundType.break_block, breakBlocks},
             {SoundType.invalid_placement, invalidPlacement},
+            {SoundType.invalid_placement2, invalidPlacement2},
+            {SoundType.invalid_placement3, invalidPlacement3},
             {SoundType.placement, placement},
+            {SoundType.placement2, placement2},
+            {SoundType.placement3, placement3},
             {SoundType.timer_tick, timerTick},
             {SoundType.timer_zero, timerHitZero},
         };
